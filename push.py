11ffca31529ce51ab2c35c920461486acb0d7b9e
bindasbartlone646-boop <bindasbartlone646@gmail.com>
2026-06-11 09:43:33 +0800

    Update push.py

--- push.py
+++ push.py
@@ -1,18 +1,23 @@
+import os
 import requests
 
-# ====== 在这里填你的信息 ======
-APP_ID = "你的AppID"
-APP_SECRET = "你的AppSecret"
-OPEN_ID = "你的OpenID"
-TEMPLATE_ID = "你的TemplateID"
+APP_ID = "wx4baca176cd4b4f17"
+APP_SECRET = os.environ.get("WX_APP_SECRET")  # 从 GitHub Secret 读取
+OPEN_ID = "oZmgn3d0JRbQqtvzfoA6ndvSwXW8"
+TEMPLATE_ID = "7aY7BaHERkr8kGikDxHtKZCaLOy9f_3rPxOLbvELP6c"
 
 def get_access_token():
-    url = f"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={APP_ID}&secret={APP_SECRET}"
+    url = (
+        f"https://api.weixin.qq.com/cgi-bin/token"
+        f"?grant_type=client_credential"
+        f"&appid={APP_ID}&secret={APP_SECRET}"
+    )
     return requests.get(url).json()["access_token"]
 
 def send_message():
     token = get_access_token()
     url = f"https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={token}"
+
     data = {
         "touser": OPEN_ID,
         "template_id": TEMPLATE_ID,
@@ -22,7 +27,9 @@ def send_message():
             "msg": {"value": "早安，今天也要加油！"}
         }
     }
-    print(requests.post(url, json=data).json())
+
+    response = requests.post(url, json=data)
+    print(response.json())
 
 if __name__ == "__main__":
     send_message()
